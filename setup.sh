8f26eb21431bc22be733967bba0abbf6f835da7e
giuliano <panzironi.giuliano@gmail.com>
2022-02-27 22:55:39 +0100

    add final note about ssh tunneling

--- setup.sh
+++ setup.sh
@@ -75,9 +75,21 @@ multipass exec master -- sudo microk8s add-node
 
 multipass exec worker -- microk8s join 10.87.132.212:25000/7c920ce61797b78f8796449dbc03dc3c/bd6bdd702477 --worker
 
+# 10. To expose a web service (e.g. k8s dashboard) from the cluster running within Multipass via its host to another client,
+# 10.1 Ensure that the service is of type NodePort
+# k -n kube-system edit service kubernetes-dashboard
+# >> type: NodePort                   # change clusterIP to NodePort (if needed)
+# 10.2 Find the high port the service is exposed to:
+#  k -n kube-system get services
+# >> kubernetes-dashboard NodePort 10.107.194.201 [none] 443:32414/TCP 20d
+# 10.3 Use a local port-forwarding to reach the cluster from your laptop using the VMs host as an SSH server
+# ssh -L local-port:multipass-master-ip:web-service-port -N -f username@ssh-server-ip
+# To male the connection permanent, create a static route.
+
 # References:
 # - https://ubuntu.com/tutorials/install-a-local-kubernetes-with-microk8s#1-overview
 # - https://pancho.dev/posts/multipass-microk8s-cluster/
 # - https://plainice.com/microk8s-bash-completion
 # - https://multipass.run/docs/additional-networks
 # - https://maurow.bitbucket.io/notes/multipass-vm-port-forwarding.html
+# - https://www.thegeekdiary.com/how-to-access-kubernetes-dashboard-externally/
