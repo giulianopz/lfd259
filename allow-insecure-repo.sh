753d9bdccd85fa34abb8d9e8b1ba31fea1916c16
giuliano <panzironi.giuliano@gmail.com>
2022-06-04 14:42:03 +0200

    fix missing double quotes

--- allow-insecure-repo.sh
+++ allow-insecure-repo.sh
@@ -8,4 +8,4 @@ sed -i  "s/# insecure = false/insecure = true/g" /etc/containers/registries.conf
 
 registry_ip=$(k get svc registry -o jsonpath='{.spec.clusterIP}')
 
-sed -i 's/# location = internal-registry-for-example.com\/bar"/location = '"$registry_ip"':5000/g' /etc/containers/registries.conf
+sed -i 's/# location = internal-registry-for-example.com\/bar"/location = "'"$registry_ip"':5000"/g' /etc/containers/registries.conf
